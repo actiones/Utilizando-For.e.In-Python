3fc28d51830ad1e0f781e43a81425476280b78e1
GatuPretu <66427859+actiones@users.noreply.github.com>
2020-12-18 11:38:14 -0300

    Create ATV 4.1 4.2 C4 PG97 For e In.py

---
+++ ATV 4.1 4.2 C4 PG97 For e In.py
@@ -0,0 +1,17 @@
+Pizzas = ['Frango c/ Catupiri', 'Mussarela', 'Calabresa']
+print('A Pizza sabor ' + Pizzas[0] + 'é irresistivel, um conjunto maravilhoso. \n' #frango catupiri
+ + 'Já a de ' + Pizzas[1] + ' é mais tradicional, por isso a gente não larga! \n' #mussarela
++ 'A de ' + Pizzas[2] + '  é Realmente mais salgadinha, mas quem disse que vou dispensar.') #calabresa
+
+for ListaPizza in Pizzas:
+    print('Você pediu ' + ListaPizza)
+    print('volte sempre a pedir ' + ListaPizza)
+ 
+print('_----____________________Atividade 4.2____________________----_')
+AnimaisSemelhantes = ['Gato', 'Tigre', 'Onça']
+print( 'Os animais abaixo Tem muito em comum, como longas velocidades, todos mamiferos e ambos são felinos!')
+for listaSemelhante in AnimaisSemelhantes:
+    print('felino: ' + listaSemelhante )
+    
+print('Todos os animais devem ser muito cultivados e respeitados.\n' +
+'existem muitos outros felinos não mencionados como Jaguatiricas, Leões e até Leopardos!')
